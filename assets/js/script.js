fa673ee98489ba1abc064b9782aa28066537746a
Mateus Larrubia <mateus.larrubia99@gmail.com>
2018-03-31 01:38:12 -0300

    Melhorias e novas implementacoes
    Melhorias de performance, adicionado fontawesome e implementadas funcoes de edicao e remocao de cadastros

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 (function (window, document) {
     "use strict";
 
+    var form = $("#form");
+    var modal = $("#modalRegister");
+    var name = $("#name");
+    var email = $("#email");
+    var date = $("#date");
+    var tableList = $("#tableList");
+
     $("#date").mask("00/00/0000");
-    var table = $("#tableList").DataTable({
+    var table = tableList.DataTable({
         language: {
             emptyTable: "Nenhum registro encontrado",
             info: "Exibindo _START_ até _END_ de _TOTAL_ registros",
@@ -32,6 +39,13 @@
             {
                 title: "Data de Nascimento",
                 data: "date"
+            },
+            {
+                title: "",
+                data: "actionButtons",
+                orderable: false,
+                width: "15%",
+                defaultContent: "<button type='button' class='btn btn-sm btn-primary btnEdit'><i class='fas fa-pencil-alt'></i> Editar</button> <button type='button' class='btn btn-sm btn-danger btnRemove'><i class='fas fa-trash'></i> Remover</button>"
             }
         ],
         data: [
@@ -48,25 +62,50 @@
         ]
     });
 
-    $("#form").submit(onSubmitHandler);
-
-    $("#modalRegister").on("hidden.bs.modal", function hiddenModalHandler(e) {
-        $(e.currentTarget).find("input").val("");
+    form.submit(function onSubmitHandler(e) {
+        e.preventDefault();
+        tableHandler();
+        table.draw();
     });
 
-    function onSubmitHandler(e) {
-        e.preventDefault();
+    modal.on("hidden.bs.modal", function hiddenModalHandler(e) {
+        var _this = $(e.currentTarget);
+        _this.find("input").val("");
+        _this.find("button[type='submit']").off("click");
+    });
 
+    tableList.on("click", ".btnRemove", function onRemoveHandler(e) {
+        if (window.confirm("Deseja remover este cadastro?")) {
+            table.row($(e.currentTarget).closest("tr")).remove().draw();
+        }
+    });
+
+    tableList.on("click", ".btnEdit", function onEditHandler(e) {
+        var row = table.row($(e.currentTarget).closest("tr"));
+        var data = row.data();
+        var button = form.find("button[type='submit']");
+        name.val(data.name || "");
+        email.val(data.email || "");
+        date.val(data.date || "");
+        modal.modal("show");
+        button.click(function beforeSubmitHandler(e) {
+            e.preventDefault();
+            if (tableHandler()) {
+                row.remove().draw();
+            }
+        });
+    });
+
+    function tableHandler() {
         if (validate()) {
             table.row.add({
-                name: $("#name").val(),
-                email: $("#email").val(),
-                date: $("#date").val()
-            }).draw();
-            $("#modalRegister").modal("hide");
+                name: name.val(),
+                email: email.val(),
+                date: date.val()
+            });
+            modal.modal("hide");
             return true;
         }
-
         return false;
     }
 
