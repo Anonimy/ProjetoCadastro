fcbe28d7901e76c7258d4490cf4bbd7f20c50c22
MateusLarrubia <mateus@publitz.com.br>
2018-04-10 17:12:09 -0300

    Melhorias na remocao de conteudo

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,11 +65,18 @@
     });
 
     $.validator.addMethod("dateBR", function dateBR(value, element) {
-        var regex = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+        var regex = /^\d{2}\/\d{2}\/\d{4}$/;
         if(value.match(regex)) {
-            var arrDate = value.split(/\//g);
-            var date = new Date(arrDate[2], Number(arrDate[1]) - 1, arrDate[0]);
-            return date < new Date();
+            var arrDate = value.split("/").map(value => {
+                return Number.parseInt(value);
+            });
+            var date = new Date(arrDate[2], (arrDate[1] - 1), arrDate[0]);
+            return (
+                date && date < new Date()
+                && date.getDate() === arrDate[0]
+                && date.getMonth() === (arrDate[1] - 1)
+                && date.getFullYear() === arrDate[2]
+            );
         }
         return false;
     });
@@ -121,9 +128,13 @@
     });
 
     tableList.on("click", ".btnRemove", function onRemoveHandler(e) {
-        if (window.confirm("Deseja remover este cadastro?")) {
+        var _thisModal = $("#modalRemove");
+        _thisModal.find("#removeConfirm").off("click").on("click", function (event) {
+            event.preventDefault();
             table.row($(e.currentTarget).closest("tr")).remove().draw();
-        }
+            _thisModal.modal("hide");
+        });
+        _thisModal.modal("show");
     });
 
     tableList.on("click", ".btnEdit", function onEditHandler(e) {
@@ -154,4 +165,4 @@
         }
         return isValidForm;
     }
-})(window, document);
+})(window, document)
